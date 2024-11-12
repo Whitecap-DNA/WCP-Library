ae30c11ae63fcbdae9a91a626ea985d1533489af
Mitch-Petersen <mitch.petersen@wcap.ca>
2024-11-12 14:45:42 -0700

    + Add DPY-4011 to oracle retry

--- WCP_Library/sql/oracle.py
+++ WCP_Library/sql/oracle.py
@@ -54,7 +54,7 @@ class SQLConnection(object):
 
         self._retry_count = 0
         self.retry_limit = 50
-        self.retry_error_codes = ['ORA-01033', 'DPY-6005']
+        self.retry_error_codes = ['ORA-01033', 'DPY-6005', 'DPY-4011']
 
     @retry
     def _connect(self) -> None:
